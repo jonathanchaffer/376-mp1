799b49f1384c989beebafef68b06fc8a7baef7ac
jonathanchaffer <jonathanchaffer@gmail.com>
2021-02-07 13:38:12 -0500

    initialize socket and make request

--- src/MP1.java
+++ src/MP1.java
@@ -1,9 +1,22 @@
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class MP1 {
-    String domain;
-    String password;
-    String ipAddress;
+    static final int DEFAULT_PORT = 443;
+    static final int TIMEOUT = 10 * 1000;
+    static final String HOST = "dynamicdns.park-your-domain.com";
+
+    private String domain;
+    private String password;
+    private String ipAddress;
+
+    private PrintStream send;
+    private DataInputStream reply;
+    private SSLSocket sock;
 
     public void promptUser() {
         Scanner scan = new Scanner(System.in);
@@ -15,8 +28,31 @@ public class MP1 {
         ipAddress = scan.nextLine();
     }
 
+    public void initializeSocket() throws IOException {
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        sock = (SSLSocket) factory.createSocket(HOST, DEFAULT_PORT);
+        if (sock != null) {
+            sock.startHandshake();
+            reply = new DataInputStream(sock.getInputStream());
+            send = new PrintStream(sock.getOutputStream());
+            sock.setSoTimeout(TIMEOUT);
+        }
+    }
+
+    public void sendRequest() {
+        send.println("GET /update?host=@&domain=" + domain + "&password=" + password + "&ip=" + ipAddress + " HTTP/1.1");
+        send.println("Host: " + HOST);
+        send.println("");
+    }
+
     public static void main(String[] args) {
-        MP1 mp1 = new MP1();
-        mp1.promptUser();
+        try {
+            MP1 mp1 = new MP1();
+            mp1.promptUser();
+            mp1.initializeSocket();
+            mp1.sendRequest();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
