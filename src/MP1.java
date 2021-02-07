579b5408c4e14715fa04cacbcb499d5219342884
jonathanchaffer <jonathanchaffer@gmail.com>
2021-02-07 13:18:29 -0500

    get user inputs

---
+++ src/MP1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MP1 {
+    String domain;
+    String password;
+    String ipAddress;
+
+    public void getUserInput() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter domain:");
+        domain = scan.nextLine();
+        System.out.println("Enter password:");
+        password = scan.nextLine();
+        System.out.println("Enter IP address:");
+        ipAddress = scan.nextLine();
+    }
+
+    public static void main(String[] args) {
+        MP1 mp1 = new MP1();
+        mp1.getUserInput();
+    }
+}
